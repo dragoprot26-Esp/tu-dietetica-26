76a6e6aa9b261cf9d92b4a9c32f7072109d6ad0a
dragoprot26-Esp <dragoprot26@gmail.com>
2026-07-30 20:45:28 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@
  *  - Supabase / fuentes / otros orígenes: pasan de largo (no se cachean).
  * Subir la versión (CACHE) limpia lo viejo al publicar cambios.
  */
-const CACHE = 'diet-v2';
+const CACHE = 'diet-v3';
 const SHELL = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
